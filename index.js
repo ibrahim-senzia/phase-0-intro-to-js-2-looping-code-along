48e78202df2aa567222ebd78824d0947f262982b
ibrahim senzia <ibrahimsenzia@gmail.com>
2024-03-23 10:32:18 +0300

    looping-code lab

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+  //Write cards()
+const writeCards = (arr,event) => {
+    const messages = []
+    for(let i = 0; i < arr.length;i++) {
+           const greetingMessage = `Thank you, ${arr[i]}, for the wonderful ${event} gift!`
+           messages.push(greetingMessage)
+    }
+    return messages
+}
+console.log(writeCards(["Charlie", "Samip", "Ali"], "birthday"))
+
+//countdown ()
+//logs each number when counting down. from the number provided
+function countDown(num) {
+    for (let i = num; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  countDown(5);
